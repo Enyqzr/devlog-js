b4d3f4b43402b87aa54f12a445d94d15213cbe67
Mathieu Degouilles <mathieu.degouilles@le-campus-numerique.fr>
2023-05-26 17:01:16 +0200

    Ex3-Complet

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,72 @@
 // you can write js here
 console.log('exo-3');
+var playerInput = prompt("Your choice ( rock, paper, scissors) ?")
+ 
+
+function getPlayerChoice(playerInput){
+    playerInput = playerInput.toLowerCase()
+    if (playerInput=="rock" || playerInput == "paper" || playerInput =="scissors" || playerInput=="zak"){
+        //console.log(playerInput)
+        return playerInput
+    }else{
+        console.log('Tu sais pas écrire recommence')
+        throw new Error("Tu sais pas écrire")
+    }
+}
+//getPlayerChoice(playerInput)
+function getComputerChoice(){
+    let Random = Math.floor(Math.random()*3)
+    
+    if(Random==0){
+        console.log("rock")
+        return "rock"
+    }
+    if(Random==1){
+        console.log("paper")
+        return "paper"
+    }
+    if(Random==2){
+        console.log("scissors")
+        return "scissors"
+    }
+}
+
+function findWinner(playerChoice,computerChoice){
+
+    if(playerChoice == computerChoice){
+        //console.log("Tied")
+        return "Tied"
+    }
+    if(playerChoice == "rock"){
+        if(computerChoice == "scissors"){
+            return "Won"
+        }else {
+            return "Lost"
+        }
+    }
+    if(playerChoice == "paper"){
+        if(computerChoice == "rock"){
+            return "Won"
+        }else{
+            return "Lost"
+        }
+    }
+    if(playerChoice == "scissors"){
+        if(computerChoice == "paper"){
+            return "Won"
+        }else{
+            return "Lost"
+        }
+    }
+    if (playerChoice == "zak"){
+        return "Magicien supreme du ballon"
+    }
+}
+//console.log(findWinner(getPlayerChoice(playerInput), getComputerChoice()))
+function playGame(){
+    let uChoice = getPlayerChoice(playerInput)
+    let computerChoice= getComputerChoice()
+    //console.log("choix humain:",uChoice,"choix ordi",computerChoice)
+    console.log(findWinner(uChoice,computerChoice))
+}
+playGame()
