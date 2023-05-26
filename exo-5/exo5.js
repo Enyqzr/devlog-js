1317bcf4b090d7176f6b6f4a08139556209424ee
Mathieu Degouilles <mathieu.degouilles@le-campus-numerique.fr>
2023-05-26 17:02:07 +0200

    Ex4-Complet

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,29 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = "numerique"
+const voyelles = ["a","e","i","o","u","y"]
+
+let resultArray = []
+/*for(let i=0; i<input.length; i++){
+   for(let j=0; j < voyelles.length; j++){
+       if (input[i] == voyelles[j]){
+            resultArray.push (voyelles[j])
+        }
+    }
+}
+
+console.log(resultArray.join("").toUpperCase()) */
+
+
+
+
+for(let i=0; i<input.length; i++){
+      if (voyelles.indexOf(input [i])> -1){
+        resultArray.push (input[i])
+      }
+    }
+
+    console.log(resultArray.join("").toUpperCase())
+   
