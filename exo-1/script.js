30ffedf281f9c770f24c9b698566656dc4c452da
Mathieu Degouilles <mathieu.degouilles@le-campus-numerique.fr>
2023-05-26 16:57:18 +0200

    Ex1-Complet

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,16 @@
 // you can write js here
-console.log('hello from file');
+console.log('hello from file'); 
+//let kelvins = 294;
+// Je declare ma variable kelvins qui me renvoie un entier
+let kelvins = prompt ("Quelle est la température en Kelvins aujourd'hui?");
+// Je declare ma variable kelvins qui me renvoie une question en direct concernant la température actuel
+let celsius = kelvins - 273;
+// Je declare ma variable celsius qui me renvoie un entier
+let Fahrenheits = celsius * (9/5) + 32;
+// Je declare ma variable Fahrenheits qui me renvoie un nombre décimal
+
+console.log(celsius);
+// Affiche moi la valeur de ma variable celsius (kelvins - 273)
+console.log(Math.floor (Fahrenheits));
+// Affiche moi la valeur de Fahreneit arrondi a un entier
+// Affiche moi la valeur de celsius
